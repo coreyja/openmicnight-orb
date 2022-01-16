94c589ea53647128a7c27b2555ec37a67f5b3b5e
Corey Alexander <coreyja@gmail.com>
2022-01-16 02:37:00 -0500

    Lots of lint fixes

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -1,17 +1,17 @@
 Setup() {
   if [ -n "$HOST" ]; then
     echo "Configuring with host: $HOST"
-    bin/omn config set host $HOST
+    bin/omn config set host "$HOST"
   fi
 
   if [ -n "$ACCOUNT_ID" ]; then
     echo "Configuring with account id: $ACCOUNT_ID"
-    bin/omn config set account_id $ACCOUNT_ID
+    bin/omn config set account_id "$ACCOUNT_ID"
   fi
 
   if [ -n "$API_TOKEN" ]; then
     echo "Setting API Token: *****"
-    bin/omn config set token $API_TOKEN
+    bin/omn config set token "$API_TOKEN"
   fi
 }
 
