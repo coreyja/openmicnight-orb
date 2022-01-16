ea52be3bf716035ea1a5cc739e1fc5eb2574ee84
Corey Alexander <coreyja@gmail.com>
2022-01-16 01:55:26 -0500

    These aren't secrets so let make sure its clear what the current config is each time

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,13 +1,20 @@
 Install() {
+  echo "Downloading latest version of OpenMicNight CLI..."
+
   local latestVersion
   local target
   local downloadHost
 
   downloadHost=${DOWNLOAD_HOST:-'https://downloads.openmicnight.dev/cli/'}
+  target=${TARGET:-'x86_64-unknown-linux-musl'}
 
+  echo "Configured download host: ${downloadHost}"
+  echo "Configured target: ${target}"
 
   latestVersion=$(curl -s "$downloadHost/latest")
-  target=${TARGET:-'x86_64-unknown-linux-musl'}
+
+  echo "Latest Version is \"$latestVersion\""
+  echo "Downloading now"
 
   mkdir -p bin
   curl -s "$downloadHost/versions/${latestVersion}/${target}/omn" -o bin/omn
